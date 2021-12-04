1211b90aa25698e0ff9df3e91e020268f3216551
Edoardo Bompiani <edoardo_bompiani@yahoo.it>
2021-12-05 00:07:48 +0100

    added user input

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,9 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
+#[derive(Component)]
+struct Player {}
+
 struct State {
     ecs: World,
 }
@@ -49,6 +52,8 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
+        player_input(self, ctx);
         self.run_systems();
 
         let position = self.ecs.read_storage::<Position>();
@@ -57,6 +62,30 @@ impl GameState for State {
         for (pos, render) in (&position, &renderable).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
+    }
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    //Player Movement
+    match ctx.key {
+        None => {} //Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
     }
 }
 
@@ -69,6 +98,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
@@ -78,6 +108,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player {})
         .build();
 
     for i in 0..10 {
