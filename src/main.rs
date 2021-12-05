d829c5034864ce71bc61289ddee167088004a201
Edoardo Bompiani <edoardo_bompiani@yahoo.it>
2021-12-05 22:38:27 +0100

    aggiunta gestione vista del giocatore

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,21 @@
+mod components;
 mod map;
 mod rect;
+mod visibility_system;
 
+use components::{Player, Position, Renderable, Viewshed};
+use map::Map;
 use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
 use specs::prelude::*;
-use specs_derive::Component;
 use std::cmp::{max, min};
-
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
-
-/*
-#[derive(Component)]
-struct LeftMover {}
-
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
-*/
-
-#[derive(Component)]
-struct Player {}
 
 struct State {
     ecs: World,
 }
-
 impl State {
     fn run_systems(&mut self) {
-        //        let mut lw = LeftWalker {};
-        //        lw.run_now(&self.ecs);
+        let mut vis = visibility_system::VisibilitySystem {};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -63,8 +29,8 @@ impl GameState for State {
 
         let position = self.ecs.read_storage::<Position>();
         let renderable = self.ecs.read_storage::<Renderable>();
-        let map = self.ecs.fetch::<Vec<map::TileType>>();
-        map::draw_map(&map, ctx);
+
+        map::draw_map(&self.ecs, ctx);
 
         for (pos, render) in (&position, &renderable).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
@@ -75,11 +41,11 @@ impl GameState for State {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<map::TileType>>();
+    let map = ecs.fetch::<Map>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = map::xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != map::TileType::Wall {
+        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map.tiles[destination_idx] != map::TileType::Wall {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
         }
@@ -116,12 +82,12 @@ fn main() -> rltk::BError {
     let mut gs = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    //    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<Viewshed>();
 
-    let (rooms, map) = map::new_map_rooms_and_corridors();
+    let map = map::Map::new_map_rooms_and_corridors();
     gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
+    let (player_x, player_y) = gs.ecs.fetch::<Map>().rooms[0].center();
 
     gs.ecs
         .create_entity()
@@ -135,20 +101,11 @@ fn main() -> rltk::BError {
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(Viewshed {
+            visible_tiles: Vec::new(),
+            range: 8,
+        })
         .build();
-
-    /*    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('☺'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(LeftMover {})
-            .build();
-    } */
 
     rltk::main_loop(context, gs)
 }
